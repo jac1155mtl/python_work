f56cfab7b2c4af6da2780cf51870ea9e68590cf0
Jo Ann Colas <44277906+jac1155mtl@users.noreply.github.com>
2021-03-10 17:19:03 -0500

    exercises for section 4.5

---
+++ _4_13_buffet.py
@@ -0,0 +1,15 @@
+# buffet menu
+foods = ('wings','pizza','ice cream','salad','spaghetti')
+print('The buffet-style restaurant offers:')
+for food in foods:
+    print(food)
+
+# try to change spaghetti to lasagna
+#foods[-1] = 'lasagna'
+#print(foods)
+
+# change 2 items in the menu
+foods = ('wings','pizza','brownies','salad','lasagna')
+print('\nThe updated menu for the buffet-style restaurant:')
+for food in foods:
+    print(food)
